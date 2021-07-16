58257c6a7ddcb21e2b07db19c46137433256c918
anandrikka <appsbyanand@gmail.com>
2021-07-16 01:12:57 -0400

    removed global context

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import useReducerWithContext from './hooks/useReducerWithMemo';
 import App from './pages';
-import { GlobalContext } from './store/define-context';
-import globalReducer, {
-  initialState as globalState,
-} from './store/reducers/root-reducer';
 import './index.css';
 
 function Root() {
-  const contextValue = useReducerWithContext(globalReducer, globalState);
   return (
     <React.StrictMode>
-      <GlobalContext.Provider value={contextValue}>
-        <Router>
-          <App />
-        </Router>
-      </GlobalContext.Provider>
+      <Router>
+        <App />
+      </Router>
     </React.StrictMode>
   );
 }
